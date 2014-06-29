069d0279acbf65100dea16a3ec23934ec6e44529
Florian Vallee (ey3ball) <florian@ey3ball.net>
2014-06-29 20:17:34 +0200

    example: add vlc load balancing sample config

--- channels_config.example.js
+++ channels_config.example.js
@@ -1,14 +1,34 @@
 vlc = require('./providers/vlc-chan');
 
 /*
- * List available channels.
+ * Basic example (one VLC server, two channels)
  *
  * Each channel is registered using a setup function that
  * returns a live stream url.
  */
-var channels = {
+var basic_config = {
         "fr2_hd": vlc.chan("FBX: France 2 HD (TNT)", "localhost"),
         "fr5_hd": vlc.chan("FBX: France 5 (HD)", "localhost")
 };
 
-module.exports = channels;
+/*
+ * Load-balancing example
+ *
+ * Two VLC instances are started, enabling the playback of two
+ * channels in parralel
+ *
+ */
+
+var vlc1 = vlc.server("localhost:80");
+var vlc2 = vlc.server("localhost:81");
+
+var vlc_pool = [ vlc1, vlc2 ];
+
+var multichan_config = {
+        "fr2_hd": vlc.chan("FBX: France 2 HD (TNT)").pool(vlc_pool),
+        "fr4_hd": vlc.chan("FBX: France 4 (HD)").pool(vlc_pool),
+        "fr5_hd": vlc.chan("FBX: France 5 (HD)").pool(vlc_pool)
+};
+
+/* use basic config example */
+module.exports = basic_config;
