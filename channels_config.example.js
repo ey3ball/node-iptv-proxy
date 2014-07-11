1157e337a8c1c70bbd4c7753a13e1324cf8b1f1e
Florian Vallee (ey3ball) <florian@ey3ball.net>
2014-07-11 21:36:07 +0200

    config: add example channels for a streamdev server

--- channels_config.example.js
+++ channels_config.example.js
@@ -1,4 +1,5 @@
 vlc = require('./providers/vlc-provider');
+streamdev = require('./providers/streamdev-provider');
 
 function make_host(host) {
         return {
@@ -10,7 +11,6 @@ function make_host(host) {
 /*
  * Two VLC instances are started, enabling the playback of two
  * channels in parralel
- *
  */
 
 var vlc1 = vlc.server(make_host("localhost:80"));
@@ -18,10 +18,17 @@ var vlc2 = vlc.server(make_host("localhost:81"));
 
 var vlc_pool = [ vlc1, vlc2 ];
 
+/*
+ * Local DVB tunners are also added through VDR+Streamdev
+ */
+var stm_host = "http://localhost:3000"
+
 var config = {
         "fr2_hd": vlc.chan("FBX: France 2 HD (TNT)").pool(vlc_pool),
         "fr4_hd": vlc.chan("FBX: France 4 (HD)").pool(vlc_pool),
-        "fr5_hd": vlc.chan("FBX: France 5 (HD)").pool(vlc_pool)
+        "fr5_hd": vlc.chan("FBX: France 5 (HD)").pool(vlc_pool),
+        "tf1hd": streamdev.chan("TF1 HD", stm_host),
+        "m6hd": streamdev.chan("M6 HD", stm_host)
 };
 
 /* use basic config example */
