db2d9f35db45b2e203b0e1c281bae7713bb23243
Florian Vallee (ey3ball) <florian@ey3ball.net>
2014-07-30 01:27:32 +0200

    test: check url provider can grab an url

--- test/test.js
+++ test/test.js
@@ -2,7 +2,8 @@ global.__base = __dirname + '/../';
 
 var assert = require("assert");
 var util = require("util");
-var IptvProvider = require(__base + "providers/iptv_provider.js");
+var IptvProvider = require(__base + "providers/iptv_provider");
+var UrlProvider = require(__base + "providers/_provider_url");
 
 describe('DummyProvider', function() {
         function DummyProvider(opts) {
@@ -17,14 +18,30 @@ describe('DummyProvider', function() {
         describe('#stop()', function() {
                 it('should fail if .start has not occured', function() {
                         assert.throws((new DummyProvider()).stop, Error);
-                })
+                });
         });
 
         describe('#start()', function() {
                 var dummy = new DummyProvider();
 
-                it('should fail when .chan has not been called', function(done) {
-                        dummy.start(function () { throw "Err" }, function () { done() });
-                })
+                it('should trigger err_cb when .chan has not been called', function(done) {
+                        dummy.start(function () { throw "FailTest" }, function () { done() });
+                });
         });
+});
+
+describe('UrlProvider', function() {
+        describe('#start()', function() {
+                it('should return a stream somehow', function(done) {
+                        (new UrlProvider("http://www.google.fr")).start(function(stream) {
+                                if (!stream)
+                                        throw "InvalidStream";
+
+                                done();
+                        }, function () {
+                                throw "FailCbCalled";
+                        });
+                });
+        });
+
 });
