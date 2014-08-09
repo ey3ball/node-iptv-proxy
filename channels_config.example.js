a0192c7755d3b80e6eb6a0ac17641a7a24b6a0cd
Florian Vallee (ey3ball) <florian@ey3ball.net>
2014-08-09 19:35:02 +0200

    move config helpers to chanlist-builder module
      fixes #8

--- channels_config.example.js
+++ channels_config.example.js
@@ -1,39 +1,7 @@
 default_profiles = require(__base + 'lib/transcoding-profiles');
+channels = require(__base + 'lib/chanlist-builder');
+
 IptvProvider = require(__base + 'providers/iptv_provider');
-
-var channels = {
-        list: [],
-        add_url: function(name, url) {
-                var prov = new IptvProvider.Url(url, { channel: name });
-
-                return this.add_provider(name, prov);
-        },
-        add_streamdev: function(name, host, channel) {
-                var prov = new IptvProvider.StreamDev(host, {channel: channel});
-
-                return this.add_provider(name, prov);
-        },
-        add_vlc: function(name, host, channel) {
-                var prov = new IptvProvider.Vlc(host.control_url, host.stream_url, {channel: channel});
-
-                return this.add_provider(name, prov);
-        },
-        add_provider: function(name, provider) {
-                if (this.list[name]) {
-                        if (this.list[name] instanceof IptvProvider.MultiSrc) {
-                                this.list[name].add(provider);
-                        } else {
-                                this.list[name] = new IptvProvider.MultiSrc();
-                        }
-                } else {
-                        this.list[name] = provider;
-                }
-                return provider;
-        },
-        make_config: function() {
-                return this.list;
-        }
-};
 
 function vlc_host(host) {
         return new IptvProvider.Vlc("http://" + host,
@@ -55,17 +23,17 @@ var streamdev_host = "http://localhost:3000";
 
 channels
         /* Streamdev source */
-        .add_streamdev  ("tf1hd", streamdev_host, "TF1 HD")
+        .addStreamdev   ("tf1hd", streamdev_host, "TF1 HD")
         /* Simple Vlc source */
-        .add_vlc        ("fr5hd", vlc1, "FBX: FRANCE5 5 (HD)")
+        .addVlc         ("fr5hd", vlc1, "FBX: FRANCE5 5 (HD)")
         /* Pooled Vlc source */
-        .add_provider   ("arte", VlcPool.bindChan("FBX: Arte (HD)")),
-        .add_provider   ("nrj12", VlcPool.bindChan("FBX: NRJ 12 (HD)"))
+        .addProvider    ("arte", VlcPool.bindChan("FBX: Arte (HD)"))
+        .addProvider    ("nrj12", VlcPool.bindChan("FBX: NRJ 12 (HD)"))
         /* Multi-source channel ! */
-        .add_provider   ("fr2hd", VlcPool.bindChan("FBX: FRANCE 2 (HD)"))
-        .add_streamdev  ("fr2hd", streamdev_host, "FRANCE2 HD")
+        .addProvider    ("fr2hd", VlcPool.bindChan("FBX: FRANCE 2 (HD)"))
+        .addStreamdev   ("fr2hd", streamdev_host, "FRANCE2 HD")
         /* Simple URL example (here with a loopback to another local channel */
-        .add_url        ("fr2_loop", "http://localhost:1234/stream/fr2hd");
+        .addUrl         ("fr2_loop", "http://localhost:1234/stream/fr2hd");
 
 /* basic config example */
 module.exports = {
@@ -75,7 +43,7 @@ module.exports = {
         },
 
         /* attach channel list */
-        channels: channels.make_config(),
+        channels: channels.genList(),
 
         /* enable transcoding with default profiles
          * we ship 3 default profiles :
